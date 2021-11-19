d3e8fdb3a4eb2763f4075279f52a7b6352a3b533
Ruslan Filonchuk <itfreshman@ukr.net>
2021-11-19 16:07:46 +0200

    Додано рішення завдання №1
    Додано рішення завдання №1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+stopBtn.disabled = true;
 
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {  
+      body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+ });
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
